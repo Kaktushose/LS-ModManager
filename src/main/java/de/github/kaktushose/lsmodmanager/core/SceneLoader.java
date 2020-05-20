19fe0b989820b119d908a0301ad02fc7e8d3719a
Kaktushose <faulie50@gmail.com>
2020-05-20 12:55:47 +0200

    fix bug in sceneloader

--- src/main/java/de/github/kaktushose/lsmodmanager/core/SceneLoader.java
+++ src/main/java/de/github/kaktushose/lsmodmanager/core/SceneLoader.java
@@ -9,17 +9,18 @@ import javafx.stage.Stage;
 
 public class SceneLoader {
 
-    private App app;
+    private final App app;
     private Stage stage;
-    private Parent root;
     private Controller controller;
 
     SceneLoader(App app) {
-        stage = new Stage();
         this.app = app;
     }
 
     public void loadFXML(Class<?> controllerClass, String file, int width, int height) {
+        Stage stage = new Stage();
+        Controller controller;
+        Parent root;
         try {
             controller = (Controller) controllerClass.getConstructor(App.class, Stage.class).newInstance(app, stage);
             FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/" + file));
@@ -30,7 +31,10 @@ public class SceneLoader {
         } catch (Exception e) { // bad practice, but this shit can throw like thousands different exceptions
             CloseEvent closeEvent = new CloseEvent(e, 1);
             closeEvent.perform();
+            return;
         }
+        this.stage = stage;
+        this.controller = controller;
     }
 
     public Controller getController() {
